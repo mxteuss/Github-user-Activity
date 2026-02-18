eb24dd741a56af714f46b68e6396f058c36888f9
mxteuss <ovagabundant@gmail.com>
2026-02-18 00:51:29 -0300

    chore: Modificando URL de requisição

--- src/main/java/com/mxteuss/GithubActivity/controller/GitHubController.java
+++ src/main/java/com/mxteuss/GithubActivity/controller/GitHubController.java
@@ -1,8 +1,7 @@
 package com.mxteuss.GithubActivity.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mxteuss.GithubActivity.model.User;
-
+import com.mxteuss.GithubActivity.model.GithubEvent;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -12,32 +11,30 @@ import java.net.http.HttpResponse;
 
 public class GitHubController {
 
-
-
     public GitHubController(String username) throws IOException, InterruptedException {
         final HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.github.com/users/" + username + "/events/public")) // URL da requisição
-                .header("Accept", "application/vnd.github+json") // Aceitar a requisição em JSON
-                .header("X-GitHub-Api-Version", "2022-11-28") //Versão da API
-                .header("User-Agent", "spring-github-cli")
+                .uri(URI.create("https://api.github.com/users/" + username + "/repos?sort=updated"))
+                .header("Accept", "application/vnd.github+json")
+                .GET()
                 .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         if (response.statusCode() == 200) {
             ObjectMapper mapper = new ObjectMapper();
-            User[] userResponse = mapper.readValue(response.body(), User[].class);
-            for (User user : userResponse) {
-                System.out.println(user.getActor().getUsername());
-                System.out.println(user.getType());
-                System.out.println(user.getRepo().getProjectName());
-                System.out.println(user.getMessage());
-                System.out.println(user.getDate());
+            GithubEvent[] userResponse = mapper.readValue(response.body(), GithubEvent[].class);
+            for (GithubEvent event1 : userResponse) {
+                System.out.println("Name: " + event1.getName());
+                System.out.println("Description: " + event1.getDescription());
+                System.out.println("Language: " + event1.getLanguage());
+                System.out.println("Last update: " + event1.getUpdatedAt());
+
                 System.out.println("----------------");
             }
         } else {
-            System.out.println("Erro da api: " + response.statusCode());
+            System.out.println("API Error: " + response.statusCode()
+            + " - " + response.body());
         }
         client.close();
     }
