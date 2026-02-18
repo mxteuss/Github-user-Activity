7958f4ba786467e3d9a56030a575265bdadbd54c
mxteuss <ovagabundant@gmail.com>
2026-02-18 00:54:38 -0300

    chore: Recebendo o usuário por input

--- src/main/java/com/mxteuss/GithubActivity/GithubActivityApplication.java
+++ src/main/java/com/mxteuss/GithubActivity/GithubActivityApplication.java
@@ -3,11 +3,15 @@ package com.mxteuss.GithubActivity;
 import com.mxteuss.GithubActivity.controller.GitHubController;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class GithubActivityApplication {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		GitHubController gitHubController = new GitHubController("mxteuss");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Type the username: ");
+		String username = sc.nextLine();
+		new GitHubController(username);
 	}
 
 }
